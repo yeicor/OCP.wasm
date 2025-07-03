05cf038312a66b4b8bd4a0c05f91fa3c47525458
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-07-03 23:56:58 +0200

    Add RapidJSON to OpenCascade build for GLTF support

--- cadquery-ocp/patch_OpenCASCADE.cmake
+++ cadquery-ocp/patch_OpenCASCADE.cmake
@@ -4,6 +4,9 @@ endif()
 if(NOT DEFINED REAL_BINARY_DIR)
   message(FATAL_ERROR "REAL_BINARY_DIR must be defined")
 endif()
+if(NOT DEFINED rapidjson_SOURCE_DIR)
+  message(FATAL_ERROR "rapidjson_SOURCE_DIR must be defined")
+endif()
 
 file(GLOB_RECURSE cmake_files
   "${REAL_SOURCE_DIR}/CMakeLists.txt"
@@ -17,9 +20,9 @@ foreach(f IN LISTS cmake_files)
   string(REPLACE "\$CMAKE_SOURCE_DIR" "${REAL_SOURCE_DIR}" content "${content}")
   string(REPLACE "\${CMAKE_BINARY_DIR}" "${REAL_BINARY_DIR}" content "${content}")
   string(REPLACE "\$CMAKE_BINARY_DIR" "${REAL_BINARY_DIR}" content "${content}")
+  string(REGEX REPLACE "(OCCT_IS_PRODUCT_REQUIRED *\\(CSF_RapidJSON +CAN_USE_RAPIDJSON\\))\n *if" "\\1\nset(3RDPARTY_RAPIDJSON_INCLUDE_DIR \"${rapidjson_SOURCE_DIR}/include\")\nif" content "${content}")
   if(NOT content STREQUAL content_old)
     file(WRITE "${f}" "${content}")
     message(STATUS "Patched ${f}")
   endif()
 endforeach()
-
