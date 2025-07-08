8cef5bcf747986d8748ca6801f97e1be4794a8d6
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-07-08 10:33:51 +0200

    Improve build123d test dependency management

--- build123d/crossplatformtricks.py
+++ build123d/crossplatformtricks.py
@@ -34,12 +34,57 @@ if sys.platform == 'emscripten':
     urllib.request.urlretrieve = _new_urlretrieve
 
 
+    # XXX: Download and install any font so that the tests can run
+    def install_font_to_ocp(font_url, font_name=None):
+        from pyodide.http import pyfetch
+        from OCP.Font import Font_FontMgr, Font_SystemFont, Font_FA_Regular
+        from OCP.TCollection import TCollection_AsciiString
+        import os, asyncio
+
+        font_name = font_name if font_name is not None else font_url.split("/")[-1]
+
+        # Choose a "system-like" font directory
+        font_path = os.path.join("/tmp", font_name)
+        os.makedirs(os.path.dirname(font_path), exist_ok=True)
+
+        # Download the font using pyfetch
+        loop = asyncio.get_event_loop()
+        response = loop.run_until_complete(pyfetch(font_url))
+        font_data = loop.run_until_complete(response.bytes())
+
+        # Save it to the system-like folder
+        with open(font_path, "wb") as f:
+            f.write(font_data)
+
+        mgr = Font_FontMgr.GetInstance_s()
+        font_t = Font_SystemFont(TCollection_AsciiString(font_path))
+        font_t.SetFontPath(Font_FA_Regular, TCollection_AsciiString(font_path))
+        assert mgr.RegisterFont(font_t, False)
+        print(f"✅ Font installed at: {font_path}")
+        return font_path
+
+
+    install_font_to_ocp("https://raw.githubusercontent.com/kavin808/arial.ttf/refs/heads/master/arial.ttf")
+
+
     def install_package(package_name: str):
-        import micropip
-        import asyncio
+        import micropip, asyncio
         # noinspection PyUnresolvedReferences
         loop = asyncio.get_event_loop()
+
+        # Ensure properly bootstrapped micropip is available (ignoring the installation of build123d itself)
+        _old_install = micropip.install
+        fut = loop.create_future()
+        fut.set_result(None)
+        micropip.install = lambda *args, **kwargs: fut
+        import bootstrap_in_pyodide  # noqa: F401
+        # Restore the original micropip.install and disable the imported bootstrap_in_pyodide
+        micropip.install = _old_install
+        sys.modules.pop("bootstrap_in_pyodide", None)
+
+        # Install the package using micropip
         loop.run_until_complete(micropip.install(package_name))
+
 
 else:
 
