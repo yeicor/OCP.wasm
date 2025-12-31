5939abab61d053de6df88572dcde206f17219e61
Yeicor <4929005+yeicor@users.noreply.github.com>
2025-12-31 10:32:43 +0100

    Force test colors and disable flaky test

--- build123d/test.py
+++ build123d/test.py
@@ -103,7 +103,7 @@ async def main():
             "--ignore=tests/test_direct_api/test_jupyter.py",
             "--ignore=tests/test_direct_api/test_vtk_poly_data.py",
             # FIXME: Work on these remaining failing/crashing tests
-            "-k=not (test_make_surface_error_checking or test_edge_wrapper_radius or test_make_surface_patch or (TestAxis and test_set) or test_tan3_2)",
+            "-k=not (test_make_surface_error_checking or test_edge_wrapper_radius or test_make_surface_patch or ((TestAxis or TestLocation) and test_set) or test_tan3_2)",
         ])
 
         # Fail on any test failure
