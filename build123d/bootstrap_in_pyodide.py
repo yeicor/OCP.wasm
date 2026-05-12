089b506be8cdc3803cfce8fdf3df042d5324f162
Yeicor <yeicor+no-reply@github.com>
2026-05-12 22:44:38 +0200

    Better PyPI setup

--- build123d/bootstrap_in_pyodide.py
+++ build123d/bootstrap_in_pyodide.py
@@ -1,27 +1,48 @@
 import importlib.metadata
 import micropip
 
+
+def _parse_first_compatible_version(spec_str):
+    for part in spec_str.split(","):
+        part = part.strip()
+        for op in (">=", "~=", "=="):
+            if part.startswith(op):
+                return part[len(op):].strip()
+    return None
+
+
+async def _mock_from_build123d_metadata():
+    try:
+        from pyodide.http import pyfetch
+        response = await pyfetch("https://pypi.org/pypi/build123d/json")
+        data = await response.json()
+        for req in data["info"].get("requires_dist", []):
+            req = req.replace("(", "").replace(")", "")
+            for pkg_name in ("cadquery-ocp-novtk", "cadquery-ocp", "lib3mf"):
+                if req.startswith(pkg_name):
+                    suffix = req[len(pkg_name):].lstrip()
+                    if not suffix or suffix[0] not in (">", "<", "=", "~", "!", "("):
+                        continue
+                    version = _parse_first_compatible_version(suffix)
+                    if version:
+                        micropip.add_mock_package(pkg_name, version)
+                    break
+    except Exception:
+        import warnings
+        warnings.warn("Failed to fetch build123d metadata from PyPI; mocks may be missing")
+
+
 async def bootstrap():
-    # build123d depends on ipython which depends on psutil and is unsupported on pyodide, so mock it
     micropip.add_mock_package("psutil", "7.2.2")
 
-    # lib3mf-OCP.wasm provides the `lib3mf` Python module (wasm port)
     await micropip.install("lib3mf-OCP.wasm")
     _version = importlib.metadata.version("lib3mf-OCP.wasm")
-    micropip.add_mock_package("lib3mf", _version)
-    # ONLY for build123d versions <0.10.0, we need to redirect the import of `py_lib3mf` to our ported `lib3mf` package.
     micropip.add_mock_package("py-lib3mf", _version, modules={"py_lib3mf": "from lib3mf import *"})
 
-    # cadquery-ocp-novtk-OCP.wasm provides the OCP Python module (wasm port)
     await micropip.install("cadquery-ocp-novtk-OCP.wasm")
-    _version = importlib.metadata.version("cadquery-ocp-novtk-OCP.wasm")
-    micropip.add_mock_package("cadquery-ocp-novtk", _version)
-    micropip.add_mock_package("ocp", _version)
 
-    # Required stdlib package, missing on Pyodide by default due to size
+    await _mock_from_build123d_metadata()
+
     await micropip.install("sqlite3")
 
-    # Ready to actually install build123d and all dependencies
     await micropip.install("build123d")
-
-    # You can now include your own build123d script, as `import build123d` will work.
