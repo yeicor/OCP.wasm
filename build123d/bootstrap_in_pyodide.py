b9467e1c4e08ad44cbfb24ccf0869c44555056e3
Yeicor <yeicor+no-reply@github.com>
2026-05-12 22:14:34 +0200

    Better PyPI setup

--- build123d/bootstrap_in_pyodide.py
+++ build123d/bootstrap_in_pyodide.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 import micropip
 
 async def bootstrap():
@@ -6,13 +7,16 @@ async def bootstrap():
 
     # lib3mf-OCP.wasm provides the `lib3mf` Python module (wasm port)
     await micropip.install("lib3mf-OCP.wasm")
+    _version = importlib.metadata.version("lib3mf-OCP.wasm")
+    micropip.add_mock_package("lib3mf", _version)
     # ONLY for build123d versions <0.10.0, we need to redirect the import of `py_lib3mf` to our ported `lib3mf` package.
-    micropip.add_mock_package("py-lib3mf", "2.4.1", modules={"py_lib3mf": "from lib3mf import *"})
+    micropip.add_mock_package("py-lib3mf", _version, modules={"py_lib3mf": "from lib3mf import *"})
 
     # cadquery-ocp-novtk-OCP.wasm provides the OCP Python module (wasm port)
     await micropip.install("cadquery-ocp-novtk-OCP.wasm")
-    # Satisfy build123d's dependency on the original package name so micropip doesn't pull the non-wasm version
-    micropip.add_mock_package("cadquery-ocp-novtk", "7.8.0")
+    _version = importlib.metadata.version("cadquery-ocp-novtk-OCP.wasm")
+    micropip.add_mock_package("cadquery-ocp-novtk", _version)
+    micropip.add_mock_package("ocp", _version)
 
     # Required stdlib package, missing on Pyodide by default due to size
     await micropip.install("sqlite3")
