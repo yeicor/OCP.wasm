0db209160e9291112a4af19e91b59a29722e3473
Yeicor <4929005+yeicor@users.noreply.github.com>
2026-05-19 11:50:32 +0200

    Add 'test_set' to ignored flaky tests in Emscripten

--- build123d/test.py
+++ build123d/test.py
@@ -144,7 +144,7 @@ async def main():
                 "--ignore=tests/test_direct_api/test_vtk_poly_data.py",  # build123d <= v0.10.0
                 # Skip some tests that are known to be flaky in the Emscripten environment, likely due to differences in floating-point behavior or other platform-specific issues. These should be investigated and fixed eventually, but for now this allows us to use tests to catch regressions in the Emscripten environment without being blocked by these known issues.
                 "-k=not ("
-                "test_tan3_2 or "
+                "test_tan3_2 or test_set or "
                 "(TestCadObjects and test_edge_wrapper_radius) or "
                 "(TestFace and test_make_surface)"
                 ")",
